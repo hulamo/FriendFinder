95ac82c52b8ac3ccedd2376db32759115a4bfb9e
Hugo Moreno <hugoladislao@gmail.com>
2019-05-31 18:06:51 -0600

    todos los archivos

---
+++ app/data/friends.js
@@ -0,0 +1,53 @@
+var friends = [
+	
+	{
+		name: "Vicente Fernández",
+		photo: "https://e00-elmundo.uecdn.es/assets/multimedia/imagenes/2016/10/26/14774816795664.jpg",
+		scores: [
+			"5",
+			"1",
+			"4",
+			"3",
+			"1",
+			"5",
+			"1",
+			"4",
+			"3",
+			"1"
+		]
+	},
+	{
+		name: "Hugo Sánchez",
+		photo: "https://www.biografiasyvidas.com/biografia/s/fotos/sanchez_hugo.jpg",
+		scores: [
+			"3",
+			"1",
+			"5",
+			"4",
+			"1",
+			"5",
+			"1",
+			"1",
+			"5",
+			"3"
+		]
+	},
+	{
+		name: "Silverter Stallone",
+		photo: "https://e00-elmundo.uecdn.es/assets/multimedia/imagenes/2019/05/24/15587164986181.jpg",
+		score: [
+			"3",
+			"1",
+			"4",
+			"1",
+			"4",
+			"3",
+			"5",
+			"1",
+			"5",
+			"4"		
+		]
+	}
+];
+
+module.exports = friends;
